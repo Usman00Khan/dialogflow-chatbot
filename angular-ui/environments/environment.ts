5dc97dd96fd8ed47aed9db7fbacefff165444427
Usman Khan <uk80718@gmail.com>
2021-02-26 11:58:38 +0530

    added

--- angular-ui/environments/environment.ts
+++ angular-ui/environments/environment.ts
@@ -16,6 +16,6 @@ export const environment = {
     production: false,
 
     dialogflow: {
-        angularBot: 'hack-mindpalace'
+        angularBot: 'ya29.c.Kp0B9Af4Wd7AIwVD0cz0IBq9CdsEH9nr_uGB4wblDCH83OppOdf01RYbuv4IsaaH2dF_vMAmRHAuL6U8Y29YZde1nDv0Dgp3q20r7TlFf5OQHLYnpgZ4EfisaFMrIAgdy-sxD71_9w8u94Fs99k4LgTNpz8zzPKyR50gVH9kbKjsQX8nH-xnN5gQummOXU0PXucbmQ9Hn523OKZ4H0aDxg'
     }
 };   
